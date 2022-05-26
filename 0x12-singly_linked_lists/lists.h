a4ca3f3d23c6093f23ea7ebc07cfaacdc1069a51
MaryBaptista <marybaptistanwachukwu@gmail.com>
2022-05-26 10:51:13 -0700

    list.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdlib.h>
-#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -24,3 +23,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
+#endif
